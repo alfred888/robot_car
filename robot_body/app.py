12b90c325418bfe71d03e9445d1052a2f26885fb
Max <wufric@163.com>
2025-06-05 00:21:10 +0800

    优化日志系统与路径，完善自动化脚本与多用户兼容，增加详细注释，提升远程运维与调试能力

--- robot_body/app.py
+++ robot_body/app.py
@@ -5,6 +5,8 @@ import yaml, os
 os.makedirs('logs', exist_ok=True)
 import logging.config
 
+curpath = os.path.realpath(__file__)
+thisPath = os.path.dirname(curpath)
 def setup_logging(config_path):
     """初始化日志配置，处理 ~ 和日志目录创建"""
     try:
